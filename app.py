0f10c57c8c2bfe2837f11fb4acaa196d06dd9c2b
Aroy <royaritra1990rocks@gmail.com>
2022-04-23 20:51:07 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -68,7 +68,8 @@ def predict():
     new_data2 = pd.get_dummies(new_data2, columns = one_hot)
     new_data1.loc[:, list(new_data1.columns)] = new_data2.loc[:, list(new_data2.columns)]
     new_data1 = new_data1.fillna(0)
-    print(new_data1)
+    print(new_data1['Sex_Female'])
+    print(new_data1['Sex_Male'])
 
     new_data1 = new_data1.astype({'BMI' : float, 'Smoking' : int,
     'AlcoholDrinking' : int,
