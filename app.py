f82eacd8fd2fb6362b36269e365111c7aaf4095f
Aroy <royaritra1990rocks@gmail.com>
2022-04-23 18:22:52 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ cols2 = ['BMI', 'Smoking', 'AlcoholDrinking', 'Stroke', 'PhysicalHealth', 'Menta
 
 @app.route('/')
 def home():
-    return render_template("Templates/index.html")
+    return render_template("index.html")
 
 @app.route('/predict', methods = ['POST'])
 def predict():
@@ -113,7 +113,7 @@ def predict():
     elif prediction == 0:
         ans = "You don't have heart disease"
 
-    return render_template('Templates/index.html', prediction_ans = ans)
+    return render_template('index.html', prediction_ans = ans)
 
 
 if __name__ == '__main__':
